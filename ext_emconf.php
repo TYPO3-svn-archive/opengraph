f94bb0c9aecc0a47277117e60743ef729c3026ef
snoy_ms <snoy_ms@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-21 11:33:52 +0000

    update config
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/opengraph/trunk@38403 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,15 @@
 <?php
 
+########################################################################
+# Extension Manager/Repository config file for ext "opengraph".
+#
+# Auto generated 21-09-2010 13:33
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Open Graph Objects',
 	'description' => 'This Extension turns your pages into Open graph objects.',
@@ -30,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:7:{s:16:"ext_autoload.php";s:4:"de69";s:12:"ext_icon.gif";s:4:"f076";s:17:"ext_localconf.php";s:4:"35d4";s:14:"ext_tables.php";s:4:"0261";s:14:"ext_tables.sql";s:4:"ed9f";s:23:"Classes/Hook/Header.php";s:4:"d457";s:43:"Legacy/class.tx_opengraph_getHeaderHook.php";s:4:"235e";}',
 	'suggests' => array(
 	),
 );
