a8a5af4457c8bb5c1e2fdfbd12ac55127c62fd98
snoy_ms <snoy_ms@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-25 19:10:05 +0000

    conf update
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/opengraph/trunk@38598 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "opengraph".
 #
-# Auto generated 21-09-2010 15:03
+# Auto generated 25-09-2010 21:08
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -13,7 +13,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Open Graph Objects',
 	'description' => 'This Extension turns your pages into Open graph objects.',
-	'category' => 'plugin',
+	'category' => 'fe',
 	'author' => 'Sascha Korth',
 	'author_company' => 'Brotherskorth',
 	'author_email' => 'sascha@brotherskorth.com',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:16:"ext_autoload.php";s:4:"de69";s:12:"ext_icon.gif";s:4:"f076";s:17:"ext_localconf.php";s:4:"35d4";s:14:"ext_tables.php";s:4:"11aa";s:14:"ext_tables.sql";s:4:"ed9f";s:23:"Classes/Hook/Header.php";s:4:"d457";s:43:"Legacy/class.tx_opengraph_getHeaderHook.php";s:4:"235e";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"7c23";s:16:"ext_autoload.php";s:4:"de69";s:12:"ext_icon.gif";s:4:"f076";s:17:"ext_localconf.php";s:4:"35d4";s:14:"ext_tables.php";s:4:"e65b";s:14:"ext_tables.sql";s:4:"ed9f";s:23:"Classes/Hook/Header.php";s:4:"d457";s:43:"Legacy/class.tx_opengraph_getHeaderHook.php";s:4:"235e";s:44:"Resources/Private/Language/locallang_csh.xml";s:4:"6bd7";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"f4f9";}',
 	'suggests' => array(
 	),
 );
